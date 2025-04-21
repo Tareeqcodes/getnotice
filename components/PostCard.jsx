2538ab16729b57d7e1d8ac2b99e8008fa60f7e8c
Devtareeq <101841038+Tareeqcodes@users.noreply.github.com>
2025-04-21 12:43:39 +0100

    Update PostCard.jsx

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export default function PostCard() {
+export default function PostCard({ post }) {
+    const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID
   return (
     <div>PostCard</div>
   )
